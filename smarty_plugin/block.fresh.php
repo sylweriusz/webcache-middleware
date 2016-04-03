ce0e561ab1b6484838ced1ae418727a29054a23a
sylweriusz <sylweriusz@gmail.com>
2016-04-04 01:42:31 +0200

    small changes

---
+++ smarty_plugin/block.fresh.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Smarty block for caching independently html parts
+ *
+ *
+ * @param                          $params
+ * @param                          $text
+ * @param Smarty_Internal_Template $template
+ * @param                          $repeat
+ *
+ * @return string
+ */
+function smarty_block_fresh($params, $text, Smarty_Internal_Template $template, &$repeat)
+{
+    if (!$repeat)
+    {
+        $id   = strip_tags($params['id']);
+        $hash = rtrim(strtr(base64_encode(hash('sha1', $_SERVER['HTTPS'] . $_SERVER['HTTP_HOST'] . $id, true)), '+/', '-_'), '=');
+        $n    = \Middleware\Webcache\Redis::box_markers($hash, $params['readonly'] ? "1" : "0");
+
+        return "\n<!-- $id -->\n" . $n[0] . "\n" . $text . "\n" . $n[1];
+    }
+}
